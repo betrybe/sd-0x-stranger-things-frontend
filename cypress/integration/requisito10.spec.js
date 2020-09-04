ba566499beec11b9fba069202d81cadef74bcee5
Gabriel <dreadpool38@gmail.com>
2020-09-04 09:53:59 -0300

    requisito 10, verificando se nao existe a tag em ambiente de produçao

--- cypress/integration/requisito10.spec.js
+++ cypress/integration/requisito10.spec.js
@@ -1,14 +1,20 @@
 /// <reference types="Cypress" />
 
 const FRONT_URL_DEV = "FRONT_URL_DEV"
+const FRONT_URL = "FRONT_URL"
 
 describe("8-Verifica se foi feito o deploy do frontend", () => {
     beforeEach(() => {
-      cy.visit(Cypress.env(FRONT_URL_DEV));
+      
     });
-    it('Verfica se a tag de desenvolvimento foi encontrada no frontend',() => {
+    it('Verfica se a tag de desenvolvimento foi encontrada no frontend de desenvolvimento',() => {
+        cy.visit(Cypress.env(FRONT_URL_DEV));
         cy.contains('Em desenvolvimento').should('exist')
     })
+    it('Verfica se a tag de desenvolvimento não existe no frontend de produção',() => {
+      cy.visit(Cypress.env(FRONT_URL));
+      cy.contains('Em desenvolvimento').should('not.exist')
+  })
   
   });
   
