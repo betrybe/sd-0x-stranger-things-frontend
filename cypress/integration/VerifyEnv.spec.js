85a41592eaf9c30f3a56f043e61334af74a8fe28
Gabriel <dreadpool38@gmail.com>
2020-09-09 17:15:07 -0300

    Aplicando algumas pequenas mudanças

--- cypress/integration/VerifyEnv.spec.js
+++ cypress/integration/VerifyEnv.spec.js
@@ -4,12 +4,12 @@ const REACT_APP_UPSIDEDOWN_URL = "REACT_APP_UPSIDEDOWN_URL";
 const REACT_APP_UPSIDEDOWN_TIMEOUT = "REACT_APP_UPSIDEDOWN_TIMEOUT";
 
 describe("Verifica as variáveis de ambiente do frontend", () => {
-  it("Verifica se as variáveis do hawkins existem", () => {
+  it("Será validado se as variáveis do hawkins existem", () => {
     expect(Cypress.env(REACT_APP_HAWKINS_URL)).to.exist;
     expect(Cypress.env(REACT_APP_HAWKINS_TIMEOUT)).to.exist;
   });
 
-  it("Verifica se as variáveis do upsidesown existem", () => {
+  it("Será validado se as variáveis do upsidesown existem", () => {
     expect(Cypress.env(REACT_APP_UPSIDEDOWN_URL)).to.exist;
     expect(Cypress.env(REACT_APP_UPSIDEDOWN_TIMEOUT)).to.exist;
   });
--- cypress/integration/developerMode.spec.js
+++ cypress/integration/developerMode.spec.js
@@ -4,11 +4,11 @@ const FRONT_URL = `https://${Cypress.env('GITHUB_USER')}-st-frontend.herokuapp.c
 const FRONT_URL_PROD = `https://${Cypress.env('GITHUB_USER')}-st-frontend-prod.herokuapp.com/.`
 
 describe("Verifica os multi-ambientes e modo de desenvolvimento", () => {
-  it("Verfica se a tag de desenvolvimento foi encontrada no frontend de desenvolvimento", () => {
+  it("Será validado que a tag de desenvolvimento foi encontrada no frontend de desenvolvimento", () => {
     cy.visit(FRONT_URL);
     cy.contains("Em desenvolvimento").should("exist");
   });
-  it("Verfica se a tag de desenvolvimento não existe no frontend de produção", () => {
+  it("Será validado que a tag de desenvolvimento não existe no frontend de produção", () => {
     cy.visit(FRONT_URL_PROD);
     cy.contains("Em desenvolvimento").should("not.exist");
   });
