0d84cf8f19ca43ee29153e2adb6d78a583b3c096
Renato Álvares <46536452+nato-re@users.noreply.github.com>
2020-12-07 08:46:51 -0300

    Apply suggestions from code review
    Aceita sugestões do @LizzardMedeiros
    
    Co-authored-by: Lizzard <luis.codemaker@gmail.com>

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const deafultTimeout = 30000;
+const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
-  constructor({ url = 'http://localhost:3000', timeout = deafultTimeout }) {
+  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
